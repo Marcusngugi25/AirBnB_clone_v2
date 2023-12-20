dc60cb3af7c2fdd7bb122b656a01d7339b4452b1
KODAK <133237061+irisvlack@users.noreply.github.com>
2023-12-21 01:32:14 +0300

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,29 @@
-#!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
-from models.user import User
+import unittest
+from datetime import datetime
+from models import *
+from console import HBNBCommand
 
 
-class test_User(test_basemodel):
-    """ """
+class Test_UserModel(unittest.TestCase):
+    """
+    Test the user model class
+    """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    def setUp(self):
+        self.cli = HBNBCommand()
+        self.model = User(email="hello@gmail.com", password="pwd")
 
-    def test_first_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+    def tearDown(self):
+        self.cli.do_destroy("User " + self.model.id)
 
-    def test_last_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+    def test_var_initialization(self):
+        self.assertTrue(hasattr(self.model, "__tablename__"))
+        self.assertEqual(self.model.__tablename__, "users")
+        self.assertTrue(hasattr(self.model, "email"))
+        self.assertTrue(hasattr(self.model, "password"))
+        self.assertTrue(hasattr(self.model, "first_name"))
+        self.assertTrue(hasattr(self.model, "last_name"))
 
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
 
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+if __name__ == "__main__":
+    unittest.main()
